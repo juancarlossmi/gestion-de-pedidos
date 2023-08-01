68d8abfadde6528e81ffdd8f4f6e074951e855a0
juancarlossmi <jcsmi@outlook.com>
2023-08-01 14:41:01 -0500

    modificar settings

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # DEBUG = True
 
 # Variable de entorno render que pone en true el desarrolo y en false si no estamos en desarrollo
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
@@ -154,6 +154,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 # Following settings only make sense on production and may break development environments.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -161,13 +162,14 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL= '/media/'
-MEDIA_ROOT= BASE_DIR / 'media'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
